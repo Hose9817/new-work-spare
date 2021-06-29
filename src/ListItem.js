0ce9bd91e2268ec82e57b7af1a31e92818552e51
Hose9817 <abdullayev6859@gmail.com>
2021-06-29 13:56:27 +0600

    confirm of delete

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,15 +1,39 @@
-function ListItem(props){
-     const {el} = props;
-    return(
+import {useState} from "react";
+
+function ListItem(props) {
+    const {el} = props;
+
+    const [confirm, setConfirm] = useState(false);
+
+    const deleteAfterConfirm = () => {
+        props.deleteCard(el._id)
+        setConfirm(false)
+    }
+
+
+    return (
         <div>
             <li>
                 {el.name}
                 {' '}
                 {el.description}
-                <button onClick={() => props.deleteCard(el._id)}>Delete</button>
+                &nbsp;
+
+                {confirm ?
+                    <>
+                        <label style={{color: 'blue'}}>Are you sure?</label>
+                        <button onClick={deleteAfterConfirm}>Yes</button>
+                        <button onClick={() => setConfirm(false)}>No</button>
+                    </>
+                    :
+                    (<button onClick={() => setConfirm(true)}>Delete</button>)
+                }
+
+
             </li>
+
         </div>
     )
 }
 
-export  default ListItem;
+export default ListItem;
